077ab5346e83f0e5b1839e35dc3fe4db8b1d9986
Aaron <aaron.crane@brightisle.ca>
2025-10-06 15:52:29 -0230

    Lexer: add basic integer literal lexer with comment and whitespace skipping
    Initial version of S-C lexer.
    - Read full file into memory
    - Skip whitespace and comments
    - Recognize integer literals
    - Emit EOF/Integer tokens

--- main.c
+++ main.c
@@ -56,9 +56,34 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+enum TokenType {
+    INT_LITERAL,
+    IDENTIFIER,
+    OPERATOR,
+    END_OF_FILE
+};
+
+struct Token {
+    int val;
+    enum TokenType type;
+    char* lexeme;
+    int line, col;
+    int length;
+};
+
+void emitToken(struct Token* token) {
+    if (token->type == INT_LITERAL) {
+        printf("INT_LITERAL: %d\n", token->val);
+    }
+    else if (token->type == END_OF_FILE) {
+        printf("END_OF_FILE\n");
+    }
+}
 
 int main() {
-    const char* fileName;
+    const char fileName[256];
     printf("Entire path to input file: \n");
 
     scanf("%s", fileName);
@@ -70,7 +95,7 @@ int main() {
     
     // Get length of file for buffer allocation
     fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file) + 1;
+    long fileSize = ftell(file);
     rewind(file);
 
     // Allocate buffer for file contents
@@ -85,36 +110,59 @@ int main() {
     buf[bytes] = '\0'; // Null-terminate the buffer
     fclose(file);
 
-    int* bp = 0; // Buffer pointer
+    int line = 1, col = 0;
+    char* bp = buf; // Buffer pointer
     while (*bp) {
-        // Skip whitespace
-        if (*bp == ' ' || *bp == '\n' || *bp == '\t') {
-            bp++;
+        if (*bp == ' ' || *bp == '\t' || *bp == '\n') {
+            if (*bp == '\n') { line++, col=0; }
+            bp++; col++;
             continue;
         }
         else if (*bp == '/') {
             // Handle comments
             if (*(bp + 1) && *(bp + 1) == '/') {
                 // Single-line comment
-                while (*bp && *bp != '\n') bp++;
+                while (*bp && *bp != '\n') { bp++; col++; }
                 continue;
             } 
             else if (*(bp + 1) && *(bp + 1) == '*') {
                 // Multi-line comment
-                bp += 2; // Skip '/*'
-                while (*bp && !(*bp == '*' && *(bp + 1) == '/')) bp++;
-                if (*bp) bp += 2; // Skip '*/'
+                bp += 2; col+=2; // Skip '/*'
+                while (*bp && !(*bp == '*' && *(bp + 1) == '/')) { bp++; col++; if (*bp == '\n') line++, col=0; }
+                if (*bp) { bp += 2; col+=2; } // Skip '*/'
                 continue;
             } 
             else {
                 // Division operator
-                bp++;
+                bp++; col++;
             }
         } 
         else {
             // Regular character
-            bp++;
+            char* start = bp;
+            int startCol = col;
+            while (isdigit(*bp)) {
+                bp++; col++;
+            }
+            if (start != bp) {
+                char* end = bp;
+                int length = end - start;
+                char* cur = start;
+
+                int tokenValue = 0;
+                while (cur < end) {
+                    tokenValue = tokenValue * 10 + (*cur - '0');
+                    cur++;
+                }
+                struct Token intToken = { .type = INT_LITERAL, .line = line, .col = startCol, .val = tokenValue, .lexeme = start, .length = length };
+                emitToken(&intToken);
+            }
+            else { bp++; col++; }
         }
     }
+
+    struct Token eofToken = { .type = END_OF_FILE, .line = line, .col = col, .val = 0, .lexeme = NULL, .length = 0 };
+    emitToken(&eofToken);
+    
     free(buf);
 }
